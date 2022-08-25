271768b3bef57b3c9a9722db5caf732c6623c543
comemaino <mat.maino@gmail.com>
2022-08-25 16:20:04 +0200

    Add: users, categories, user_category tables

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,10 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('business_name')->unique();
+            $table->string('address');
+            $table->string('vat', 11)->unique();
+            $table->text('cover')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
