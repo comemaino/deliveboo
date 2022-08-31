370345f55281dc0496ce5bf7fd80ccfa8d32162a
bodascaluca <bodasca.luca@gmail.com>
2022-08-31 21:01:04 +0200

    front.js che ho dovuto pushare

--- public/js/front.js
+++ public/js/front.js
@@ -58319,7 +58319,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\fede1\Documents\Boolean\deliveboo\resources\js\front.js */"./resources/js/front.js");
+module.exports = __webpack_require__(/*! /Users/lucabodasca/Downloads/BOOLEAN/Boolean-Laravel/Deliveboo/deliveboo/resources/js/front.js */"./resources/js/front.js");
 
 
 /***/ })
