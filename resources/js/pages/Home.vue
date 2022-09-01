e7b783ee8c9a7b1a960ab52a73f9e45f8a5473bb
comemaino <mat.maino@gmail.com>
2022-09-01 18:06:47 +0200

    Add: client-side validation

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,52 +1,48 @@
 <template>
-  <div>
-    <NavBar :categories="categories" @selectedCategory="getRestaurants($event)" />
-    <h1>Home</h1>
-    <AppRestaurants :restaurants="restaurants" />
-  </div>
+    <div>
+        <NavBar
+            :categories="categories"
+            @selectedCategory="getRestaurants($event)"
+        />
+        <h1>Home</h1>
+        <AppRestaurants :restaurants="restaurants" />
+    </div>
 </template>
-
 
 <script>
 import axios from "axios";
-import AppRestaurants from '../components/AppRestaurants.vue';
+import AppRestaurants from "../components/AppRestaurants.vue";
 import NavBar from "../components/NavBar.vue";
 export default {
-  name: "Home",
-  components: {
-    AppRestaurants,
-    NavBar,
-  },
-  data() {
-    return {
-      restaurants: [],
-      categories: [],
-    };
-  },
-  created() {
-    this.getRestaurants(17);
-  },
-  methods: {
-    getRestaurants(id) {
-      axios
-        .get(`/api/${id}`)
-        .then((resp) => {
-          const results = resp.data.results.restaurants;
-          console.log(typeof results);
-          this.restaurants = resp.data.results.restaurants;
-          // if(typeof resp.data.results.restaurants === 'array') {
-          // } else {
-          //   this.restaurants = [];
-          //   this.restaurants.push(resp.data.results.restaurants);
-          // }
-          this.categories = resp.data.results.categories;
-          console.log(resp.data.results);
-        });
+    name: "Home",
+    components: {
+        AppRestaurants,
+        NavBar,
     },
-    
-  }
+    data() {
+        return {
+            restaurants: [],
+            categories: [],
+        };
+    },
+    created() {
+        this.getRestaurants(17);
+    },
+    methods: {
+        getRestaurants(id) {
+            axios.get(`/api/${id}`).then((resp) => {
+                this.restaurants = resp.data.results.restaurants;
+                // if(typeof resp.data.results.restaurants === 'array') {
+                // } else {
+                //   this.restaurants = [];
+                //   this.restaurants.push(resp.data.results.restaurants);
+                // }
+                this.categories = resp.data.results.categories;
+                console.log(resp.data.results);
+            });
+        },
+    },
 };
 </script>
 
-<style>
-</style>
+<style></style>
