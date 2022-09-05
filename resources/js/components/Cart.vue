8cd0f044609dbdd560363538a121fb6e89240e91
Cix8 <francesco.natal_fn@libero.it>
2022-09-05 12:00:24 +0200

    add localStorage.cart, add addCart() & resetCart() functions, productQuantity_logic improved

--- resources/js/components/Cart.vue
+++ resources/js/components/Cart.vue
@@ -3,13 +3,24 @@
     <h3>Il tuo carrello</h3>
     <div class="products-list">
       <ul>
-        <li v-for="product in products" :key="product.id">
-          <span>{{product.name}}</span>
-          <input type="number" name="product_quantity" id="product-quantity" value="product_quantity"/>
-          <span>€ {{product.price}}  {{product.quantity}}</span>
+        <li v-for="(product, index) in theseProducts" :key="index">
+          <span>{{ product.name }}</span>
+          <input
+            type="number"
+            name="product_quantity"
+            :id="'product-quantity-'+ product.id"
+            :value="product.productQuantity"
+            @change="addMore(product), getAmount()"
+          />
+          <span class="amount">€ {{ product.price * product.productQuantity }}</span>
         </li>
       </ul>
-      <h5>Totale: ∑ product_price * product_quantity</h5>
+      <h5>Totale: € {{ cartAmount }}</h5>
+    </div>
+    <div class="">
+      <button class="btn btn-danger" @click="resetCart()">
+        Svuota carrello
+      </button>
     </div>
   </div>
 </template>
@@ -17,10 +28,39 @@
 <script>
 export default {
   name: "Cart",
+  data() {
+    return {
+      theseProducts: [],
+      cartAmount: 0,
+    };
+  },
+  mounted() {
+    if (localStorage.cart) {
+      console.log(JSON.parse(localStorage.cart));
+      const thisCart = JSON.parse(localStorage.cart);
+      this.theseProducts = thisCart.productsArray;
+    }
 
-  props: {
-        products: Array
+    this.getAmount();
+  },
+  methods: {
+    resetCart() {
+      localStorage.cart = "";
     },
+    addMore(product) {
+      const quantity = document.getElementById('product-quantity-'+ product.id).value;
+      product.productQuantity = quantity;
+    },
+    getAmount() {
+      let amount = 0;
+      this.theseProducts.forEach((product) => {
+        let result = 0;
+        result = product.price * product.productQuantity
+        amount += result;
+      })
+      this.cartAmount = parseFloat(amount.toFixed(2));
+    }
+  },
 };
 </script>
 
--- resources/js/components/RestaurantMenu.vue
+++ resources/js/components/RestaurantMenu.vue
@@ -1,46 +1,49 @@
 <template>
-	<div>
-		<template v-if="loading">
-			<div class="row">
-				<div class="col-9">
-					<div>
-						<h2>Menù di {{ restaurant.business_name }}</h2>
-						<div class="row row-cols-4">
-							<div v-for="product in products" :key="product.id">
-								<div class="card mb-2" style="width: 18rem">
-									<img
-										class="card-img-top"
-										:src="'../storage/' + product.img"
-										alt="Card image cap"
-									/>
-									<div class="card-body">
-										<h5 class="card-title">{{ product.name }}</h5>
-										<p class="card-text">{{ product.description }}</p>
-										<p class="card-text">{{ product.ingredients }}</p>
-										<div class="add">
-											<span>€ {{ product.price }}</span>
-											<input
-												type="number"
-												name="product_quantity"
-												:id="'input' + product.id"
-												value=""
-											/>
-											<button class="btn btn-success" @click="addToCart(product.id, product.user_id)">
-												Aggiungi al carrello
-											</button>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col-3">
-					<Cart />
-				</div>
-			</div>
-		</template>
-	</div>
+  <div>
+    <template v-if="loading">
+      <div class="row">
+        <div class="col-9">
+          <div>
+            <h2>Menù di {{ restaurant.business_name }}</h2>
+            <div class="row row-cols-4">
+              <div v-for="product in products" :key="product.id">
+                <div class="card mb-2" style="width: 18rem">
+                  <img
+                    class="card-img-top"
+                    :src="'../storage/' + product.img"
+                    alt="Card image cap"
+                  />
+                  <div class="card-body">
+                    <h5 class="card-title">{{ product.name }}</h5>
+                    <p class="card-text">{{ product.description }}</p>
+                    <p class="card-text">{{ product.ingredients }}</p>
+                    <div class="add">
+                      <span>€ {{ product.price }}</span>
+                      <input
+                        type="number"
+                        name="product_quantity"
+                        :id="'input' + product.id"
+                        value=""
+                      />
+                      <button
+                        class="btn btn-success"
+                        @click="addToCart(product)"
+                      >
+                        Aggiungi al carrello
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-3">
+          <Cart />
+        </div>
+      </div>
+    </template>
+  </div>
 </template>
 
 <script>
@@ -48,51 +51,74 @@ import axios from "axios";
 import Cart from "./Cart.vue";
 
 export default {
-	name: "RestaurantMenu",
-	components: {
-		Cart,
-	},
-	data() {
-		return {
-			loading: false,
-			restaurant: null,
-			products: [],
-			cartProducts: [ 
-        // {id : undefined,
-        // user_id : undefined,
-        // productQuantity : undefined}
+  name: "RestaurantMenu",
+  components: {
+    Cart,
+  },
+  data() {
+    return {
+      loading: false,
+      restaurant: null,
+      products: [],
+      cart: {
+        productsArray: [],
+      },
+    };
+  },
+  created() {
+    this.getProducts();
+    // console.log(this.cartProducts);
+  },
+  mounted() {
+    if (localStorage.cart) {
+      // const cartAsArray = localStorage.cart.split(",");
+      const cartAsArray = JSON.parse(localStorage.cart);
+      console.log(cartAsArray);
+      this.cart = cartAsArray;
+      // localStorage.cart = '';
+      // console.log(this.cartProducts);
+    }
+  },
+  methods: {
+    getProducts() {
+      const slug = this.$route.params.slug;
+      axios.get(`/api/${slug}/menu`).then((resp) => {
+        // console.log(resp.data.restaurant);
+        this.restaurant = resp.data.restaurant;
+        this.products = resp.data.products;
+        this.loading = true;
+      });
+    },
 
-      ],
-		};
-	},
-	created() {
-		this.getProducts();
-    console.log(this.cartProducts);
-	},
-	methods: {
-		getProducts() {
-			const slug = this.$route.params.slug;
-			axios.get(`/api/${slug}/menu`).then((resp) => {
-				// console.log(resp.data.restaurant);
-				this.restaurant = resp.data.restaurant;
-				this.products = resp.data.products;
-				this.loading = true;
-			});
-		},
-
-		addToCart(id, user_id) {
-
+    addToCart(item) {
       // if (this.cartProducts.length > 0 && this.cartProducts[0].user_id !== user_id) {
       //   alert('Non puoi aggiungere articoli di ristoranti diversi')
       // } else {
-			  let productQuantity = parseInt(document.getElementById("input" + id).value);
-        // if (product.user_id)
-			  this.cartProducts.push([id, user_id, productQuantity]);
-        document.getElementById("input" + id).value = null;
-			  console.log(this.cartProducts);
-      
-		},
-	},
+      // if (product.user_id)
+      let checkResult = this.cart.productsArray.filter((element) => element.id === item.id ? true : false);
+      if (checkResult.length !== 0) {
+        checkResult[0].productQuantity++;
+        console.log(checkResult[0]);
+      } else {
+        let productQuantity;
+        if (!document.getElementById("input" + item.id).value) {
+          productQuantity = 1;
+        } else {
+          productQuantity = parseInt(document.getElementById("input" + item.id).value);
+        }
+        // this.cartProducts.push([id, user_id, productQuantity]);
+        const product = item;
+        item.productQuantity = productQuantity;
+        this.cart.productsArray.push(product);
+      }
+
+      // document.getElementById("input" + id).value = null;
+      // console.log(this.cartProducts);
+      // console.log(JSON.stringify(product));
+      localStorage.cart = JSON.stringify(this.cart);
+      console.log(localStorage.cart);
+    },
+  },
 };
 </script>
 
