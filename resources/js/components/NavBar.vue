d55667b1b9e476748251ff072f6ae4e7b45e5123
comemaino <mat.maino@gmail.com>
2022-09-06 11:49:00 +0200

    Style: cart buttons

--- resources/js/components/NavBar.vue
+++ resources/js/components/NavBar.vue
@@ -1,10 +1,10 @@
 <template>
     <nav>
-        <div class="form-check form-check-inline" v-for="category in categories" :key="category.id">
-            <span @click="getSelectedCategory(category.id)" :class=" categories_id.includes(category.id) ? 'active' : ''">{{ category.name }}</span>
-        </div>
         <div class="form-check form-check-inline">
             <span @click="getSelectedCategory(null)" :class=" categories_id.length === 0 ? 'active' : ''">tutti</span>
+        </div>
+        <div class="form-check form-check-inline" v-for="category in categories" :key="category.id">
+            <span @click="getSelectedCategory(category.id)" :class=" categories_id.includes(category.id) ? 'active' : ''">{{ category.name }}</span>
         </div>
     </nav>
 </template>
