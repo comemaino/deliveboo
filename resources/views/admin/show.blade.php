55a3c80302022c6bc0c8c8f01528eae1572f4c89
Cix8 <francesco.natal_fn@libero.it>
2022-09-07 09:36:43 +0200

    add empty_menu_message

--- resources/views/admin/show.blade.php
+++ resources/views/admin/show.blade.php
@@ -7,24 +7,33 @@
         </div>
     @endif
 
-    <div class="container">
-        <div class="row row-cols-3">
 
-            @foreach ($products as $product)
-                <div class="card" style="width: 18rem">
-                    @if ($product->img)
-                        <img src="{{ asset('storage/' . $product->img) }}" class="card-img-top" alt="...">
-                    @endif
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $product->name }}</h5>
-                        <p class="card-text">{{ $product->ingredients }}</p>
-                        <a href="{{ route('admin.products.show', ['slug' => $product->slug]) }}"
-                            class="btn btn-primary">Dettagli Piatto</a>
+    @if (count($products) !== 0)
+        <div class="container">
+            <div class="row row-cols-3">
+                @foreach ($products as $product)
+                    <div class="card" style="width: 18rem">
+                        @if ($product->img)
+                            <img src="{{ asset('storage/' . $product->img) }}" class="card-img-top" alt="...">
+                        @endif
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $product->name }}</h5>
+                            <p class="card-text">{{ $product->ingredients }}</p>
+                            <a href="{{ route('admin.products.show', ['slug' => $product->slug]) }}"
+                                class="btn btn-primary">Dettagli Piatto</a>
+                        </div>
                     </div>
-                </div>
-            @endforeach
+                @endforeach
+            </div>
 
         </div>
+    @else
+        <div class="text-center py-5">
+            <h3>Il tuo menù è vuoto, <a class="link-primary text-decoration-none"
+                    href="{{ route('admin.products.create') }}">aggiungi il tuo
+                    primo prodotto</a></h3>
+        </div>
+    @endif
 
-    </div>
+
 @endsection
