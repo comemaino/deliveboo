07b2886f9d60687d3412150754411a9ab796e2a9
Fede180600 <fede180600@libero.it>
2022-09-09 17:57:34 +0200

    mail to admin & guest fixed with order data & customer data

--- resources/views/mails/send_mail_to_admin.blade.php
+++ resources/views/mails/send_mail_to_admin.blade.php
@@ -11,6 +11,13 @@
 <body>
 
     <h1>Hai ricevuto un nuovo ordine</h1>
+    <div>
+        <p>Totale dell'ordine: {{ $new_order->amount }}</p>
+        <p>Ordine effettuato in data: {{ $new_order->created_at }}</p>
+        <p>Nome completo cliente: {{ $new_order->customer_fullname }}</p>
+        <p>Email cliente: {{ $new_order->customer_email }}</p>
+        <p>Indirizzo cliente: {{ $new_order->customer_address }}</p>
+    </div>
     <a href="{{ route('admin.orders.index') }}">Accedi alla tua area riservata per visualizzare il dettaglio ordini</a>
 
 </body>
--- resources/views/mails/send_mail_to_guest.blade.php
+++ resources/views/mails/send_mail_to_guest.blade.php
@@ -11,6 +11,31 @@
 <body>
 
     <h1>Hai effettuato un nuovo ordine</h1>
+    <div>
+        <p>Ordine effettuato in data: {{ $new_order->created_at }}</p>
+        <p>Nome sul campanello: {{ $new_order->customer_fullname }}</p>
+        <p>Indirizzo di spedizione: {{ $new_order->address }}</p>
+        <h3>Hei acquistato:</h3>
+        <table class=" table table-light table-striped">
+				<thead class="px-2">
+					<tr>
+						<th scope="col">Nome Prodotto</th>
+						<th scope="col">Quantità</th>
+						<th scope="col">Prezzo (€)</th>
+					</tr>
+				</thead>
+				<tbody>
+					@foreach ($products as $product)
+						<tr>
+							<th class="card-title">{{ $product->name }}</th>
+							<td class="card-text">{{ $product->quantity }}</td>
+							<td class="card-text">{{ $product->price }}</td>
+						</tr>
+					@endforeach
+				</tbody>
+			</table>
+        <p>Totale dell'ordine: {{ $new_order->amount }}</p>
+    </div>
 
 </body>
 
